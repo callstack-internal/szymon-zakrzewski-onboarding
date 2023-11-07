cd89b5c37bea7ba4a5bdb86201d97792f6f01001
Szymon Zakrzewski <szymon.zakrzewski@callstack.com>
2023-11-07 17:58:20 +0100

    feat: add BadgeLabel component

---
+++ src/styles/colors.ts
@@ -0,0 +1,21 @@
+const constColors = {
+  white: '#ffffff',
+  black: '#2E3035',
+  gray: '#708090',
+};
+
+const colorPalette = {
+  ...constColors,
+  accent: '#4682B4',
+};
+
+export default {
+  badgeLabel: {
+    text: colorPalette.white,
+    background: colorPalette.accent,
+  },
+  text: {
+    primary: colorPalette.black,
+    secondary: colorPalette.gray,
+  },
+};
