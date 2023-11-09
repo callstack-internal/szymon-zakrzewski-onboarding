a31fd145810c6e60eb5475fa639149e86026f919
Szymon Zakrzewski <szymon.zakrzewski@callstack.com>
2023-11-09 18:38:54 +0100

    feat: add city weather list on home screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {QueryClient, QueryClientProvider} from 'react-query';
+
 import AppNavigator from './src/navigation/AppNavigator';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
 
--- src/screens/homeScreen/HomeScreen.tsx
+++ src/screens/homeScreen/HomeScreen.tsx
@@ -1,21 +1,79 @@
-import React from 'react';
-import {Button, View} from 'react-native';
+import React, {useMemo} from 'react';
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  ViewStyle,
+} from 'react-native';
 
 import useNavigation from 'app/navigation/useNavigation';
-import {translations} from 'app/locales/translations';
-import common from 'app/styles/common';
 import {Routes} from 'app/navigation/constants';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import CityWeatherList from './components/CItyWeatherList';
+
+import common from 'app/styles/common';
+import {translations} from 'app/locales/translations';
+import constants from 'app/constants';
+import colors from 'app/styles/colors';
+
+import {useWeatherForCities} from './hooks/useWeatherForCities';
 
 function HomeScreen(): JSX.Element {
   const navigation = useNavigation();
+  const insets = useSafeAreaInsets();
+  const {isLoading, data, error, refetch} = useWeatherForCities();
+
+  const {listContentContainerStyle, centeredWithBottomInset} = useMemo<{
+    listContentContainerStyle: ViewStyle;
+    centeredWithBottomInset: ViewStyle;
+  }>(
+    () => ({
+      listContentContainerStyle: {
+        paddingBottom: insets.bottom,
+      },
+      centeredWithBottomInset: {
+        ...common.centered,
+        paddingBottom: insets.bottom,
+      },
+    }),
+    [insets],
+  );
+
+  if (isLoading) {
+    return <ActivityIndicator size="large" style={centeredWithBottomInset} />;
+  }
+
+  if (error || !data) {
+    return (
+      <View style={centeredWithBottomInset}>
+        <Text style={styles.textTitle}>
+          {translations.homeScreen.error.message}
+        </Text>
+        <Button title={translations.common.retry} onPress={refetch} />
+      </View>
+    );
+  }
+
   return (
-    <View style={common.centered}>
-      <Button
-        title={translations.screenTitle.details}
-        onPress={() => navigation.navigate(Routes.details)}
+    <View>
+      <CityWeatherList
+        data={data}
+        onPress={item => {
+          navigation.navigate(Routes.details, {...item});
+        }}
+        contentContainerStyle={listContentContainerStyle}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  textTitle: {
+    fontSize: constants.text.base.size,
+    color: colors.text.primary,
+  },
+});
 
 export default HomeScreen;
