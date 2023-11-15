34f7c198409c2ba0352d57bc34bd5fcc804a1b31
Szymon Zakrzewski <szymon.zakrzewski@callstack.com>
2023-11-15 20:51:57 +0100

    feat: sort cities by location

---
+++ src/screens/homeScreen/hooks/useCurrentLocation.tsx
@@ -0,0 +1,42 @@
+import {useCallback, useEffect, useRef, useState} from 'react';
+
+import LocationModule, {Location} from 'app/native/Location';
+
+type UseCurrentLocation = {
+  location?: Location;
+  refreshLocation: () => void;
+};
+
+export function useCurrentLocation(): UseCurrentLocation {
+  const [currentLocation, setCurrentLocation] = useState<Location>();
+  const mounted = useRef(false);
+
+  const refresh = useCallback(() => {
+    LocationModule.getCurrentLocation()
+      .then(location => {
+        if (!mounted.current) {
+          return;
+        }
+        setCurrentLocation(location);
+      })
+      .catch(locationError => {
+        console.warn(
+          'useCurrentLocation: could not get current location',
+          locationError,
+        );
+      });
+  }, []);
+
+  useEffect(() => {
+    mounted.current = true;
+    refresh();
+    return () => {
+      mounted.current = false;
+    };
+  }, [refresh]);
+
+  return {
+    location: currentLocation,
+    refreshLocation: refresh,
+  };
+}
--- src/screens/homeScreen/hooks/useWeatherForCities.tsx
+++ src/screens/homeScreen/hooks/useWeatherForCities.tsx
@@ -7,6 +7,30 @@ import {APIError} from 'app/api/Error';
 import constants from 'app/constants';
 import {useRefCallback} from 'app/hooks/useRefCallback';
 import {CityListItem} from 'app/screens/homeScreen/components/CItyWeatherList';
+import {useCurrentLocation} from './useCurrentLocation';
+import {Location} from 'app/native/Location';
+import {useEffect, useState} from 'react';
+
+type CityListItemWithLocation = CityListItem & {location: Location};
+
+// We should calculate the actual distance, not the absolute differences
+// in latitude and longitude, but let's keep it simple for now.
+const sortByLocation = (
+  target: Location,
+  items: CityListItemWithLocation[],
+): CityListItemWithLocation[] => {
+  return items.sort(({location: lhsLocation}, {location: rhsLocation}) => {
+    const lhsLatitudeDiff = Math.abs(target.latitude - lhsLocation.latitude);
+    const lhsLongitudeDiff = Math.abs(target.longitude - lhsLocation.longitude);
+    const lhsDiff = lhsLatitudeDiff + lhsLongitudeDiff;
+
+    const rhsLatitudeDiff = Math.abs(target.latitude - rhsLocation.latitude);
+    const rhsLongitudeDiff = Math.abs(target.longitude - rhsLocation.longitude);
+    const rhsDiff = rhsLatitudeDiff + rhsLongitudeDiff;
+
+    return lhsDiff - rhsDiff;
+  });
+};
 
 type UseWeatherForCities = {
   isLoading: boolean;
@@ -24,7 +48,7 @@ export function useWeatherForCities(
     data,
     error,
     refetch: _refetch,
-  } = useQuery<GroupWeather, APIError | Error, CityListItem[]>({
+  } = useQuery<GroupWeather, APIError | Error, CityListItemWithLocation[]>({
     queryKey: ['weatherForCities'],
     queryFn: context =>
       queryFn
@@ -36,8 +60,22 @@ export function useWeatherForCities(
         city: item.name,
         weather: item.weather[0]?.main || '',
         temperature: `${item.main.temp}`,
+        location: {
+          latitude: item.coord.lat,
+          longitude: item.coord.lon,
+        },
       })),
   });
+
+  const {location} = useCurrentLocation();
+  const [sortedData, setSortedData] = useState<CityListItem[]>();
+
+  useEffect(() => {
+    if (!data || !location) {
+      return;
+    }
+    setSortedData(sortByLocation(location, data.slice()));
+  }, [data, location]);
 
   const refetch = useRefCallback(() => {
     if (isFetching) {
@@ -50,7 +88,7 @@ export function useWeatherForCities(
 
   return {
     isLoading,
-    data,
+    data: sortedData || data,
     error,
     refetch,
   };
